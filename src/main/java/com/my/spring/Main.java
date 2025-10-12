2879e603c6203a931effcb75d3a49e3d54aa674c
pancham01 <pancham9557@gmail.com>
2025-10-12 10:33:52 +0530

    Initial commit

---
+++ src/main/java/com/my/spring/Main.java
@@ -0,0 +1,18 @@
+package com.my.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.my.spring.model.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		Employee bean = ioc.getBean("emp2",Employee.class);
+		
+		System.out.println(bean);
+		
+	}
+
+}
---
+++ src/main/java/com/my/spring/model/Employee.java
@@ -0,0 +1,71 @@
+package com.my.spring.model;
+
+public class Employee {
+
+	private int id;
+	private String name,gender;
+	
+	private Address address;
+	
+	public Employee() {
+		super();
+		System.out.println("Employee.Employee()");
+	}
+	
+	
+	public Employee(int id, String name, String gender, Address address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.address = address;
+		System.out.println("Employee.Employee(4)");
+	}
+
+
+
+
+	public Address getAddress() {
+		return address;
+	}
+
+
+
+
+	public void setAddress(Address address) {
+		this.address = address;
+		System.out.println("Employee.setAddress()");
+	}
+
+
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		System.out.println("Employee.setId()");
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		System.out.println("Employee.setName()");
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		System.out.println("Employee.setGender()");
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + "]";
+	}
+	
+	
+}
